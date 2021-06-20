23f35255f7197a2741a97232506fa3c0f6a78a66
Hema-BY <85074864+Hema-BY@users.noreply.github.com>
2021-06-20 18:16:38 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,50 @@
+<!-- navbar --> 
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <style type="text/css">
+
+        body{margin-top: 15px;
+        }
+       
+          .btn {
+                  border: 3px solid black;
+                  background-color: #A4A4A4;
+                  color: black;
+                  padding: 14px 28px;
+                  font-size: 16px;
+                  cursor: pointer;
+                  margin: 5px;
+                }
+            
+            .df:hover {
+        color: white;
+        font-size: 16px;
+    background-color: #5858FA;
+background-image: linear-gradient(315deg, #A4A4A4 0%, #5e5c5c 74%);
+
+}
+
+        </style>
+      </head>
+      <body>
+      
+      <nav  class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php"><img src="img/logo.PNG"style="width:50%; height:50%;"></a>
+      
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="index.php">Home</a>
+              </li>
+              
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="aboutme.php">About Me</a>
+              </li>
+              <li class="nav-item">
+                <a class="btn df" class="nav-link" href="contact.php">Contact Me</a>
+              </li>
+          </div>
+       </nav>
+     </body>
+     </html>
